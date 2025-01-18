5f4180284e90f847c740475c1bc7623ca61ea9c4
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-01-18 18:25:53 +0100

    preprocess each .md file with vto

--- _config.ts
+++ _config.ts
@@ -4,4 +4,12 @@ const site = lume({
     src: "src",
 });
 
+
+// Every MD file accepts VTO variables.
+site.preprocess([".md"], (pages) => {
+    for (const page of pages){
+        page.data.templateEngine = ["vto", "md"];
+    }
+});
+
 export default site;
