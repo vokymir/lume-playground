afcc8b4665a711408e58b12091a8cf6b967b352f
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-01-22 15:20:46 +0100

    add date plugin

--- _config.ts
+++ _config.ts
@@ -2,6 +2,8 @@ import lume from "lume/mod.ts";
 import basePath from "lume/plugins/base_path.ts";
 import nav from "lume/plugins/nav.ts"; // used in _components/header.vto
 import relations from "lume/plugins/relations.ts";
+import date from "lume/plugins/date.ts";
+import { cs } from "npm:date-fns/locale/cs";
 
 const site = lume({
     src: "src",
@@ -24,7 +26,11 @@ site.use(basePath())
             basic_page: "page_id",
             author: "author_id",
             leader: "leader_id",
+            camp: "camp_id",
         }
+    }))
+    .use(date({
+        locales: { cs },
     }));
 
 // Include the pico style
