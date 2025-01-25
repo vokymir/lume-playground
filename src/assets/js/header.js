61bc5363e69114ea9779d666f6533d89e04e8e4c
Jakub Vokoun <22869857+vokymir@users.noreply.github.com>
2025-01-25 16:56:20 +0000

    remove css and js from header and structure it. also bulid better menu

---
+++ src/assets/js/header.js
@@ -0,0 +1,30 @@
+// Search overlay
+document.getElementById('search-trigger').onclick = function() {
+    document.getElementById('search-overlay').style.display = 'block';
+  }
+
+  document.getElementById('search-overlay').onclick = function(e) {
+    if (e.target === this) {
+      this.style.display = 'none';
+    }
+  }
+
+
+/* THEME SWITCH */
+  const themeSwitch = document.getElementById('theme-switcher');
+  
+  if (themeSwitch) {
+    themeSwitch.addEventListener('click', (e) => {
+      e.preventDefault();
+      const currentTheme = document.documentElement.getAttribute('data-theme');
+      const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+      
+      document.documentElement.setAttribute('data-theme', newTheme);
+      localStorage.setItem('theme', newTheme);
+    });
+  }
+
+  // Set initial theme from localStorage or system preference
+  const savedTheme = localStorage.getItem('theme') || 
+    (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+  document.documentElement.setAttribute('data-theme', savedTheme);
